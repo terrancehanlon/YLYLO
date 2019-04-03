cbf71948ed5524189803120b311d1bab0d368a49
Terrance <terrance.hanlon@ndsu.edu>
2019-04-02 21:34:06 -0500

    eclipse gitignore

--- src/main/java/com/YLYLO/entites/City.java
+++ src/main/java/com/YLYLO/entites/City.java
@@ -6,5 +6,38 @@ public class City {
 	State state;
 	Zip zip;
 	String name;
+	public Country getCountry() {
+		return country;
+	}
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+	public State getState() {
+		return state;
+	}
+	public void setState(State state) {
+		this.state = state;
+	}
+	public Zip getZip() {
+		return zip;
+	}
+	public void setZip(Zip zip) {
+		this.zip = zip;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public City(Country country, State state, Zip zip, String name) {
+		super();
+		this.country = country;
+		this.state = state;
+		this.zip = zip;
+		this.name = name;
+	}
+	
+	
 
 }
--- src/main/java/com/YLYLO/entites/State.java
+++ src/main/java/com/YLYLO/entites/State.java
@@ -7,6 +7,30 @@ public class State {
 	Country country;
 	List<Zip> zipcodes;
 	String name;
+	public Country getCountry() {
+		return country;
+	}
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+	public List<Zip> getZipcodes() {
+		return zipcodes;
+	}
+	public void setZipcodes(List<Zip> zipcodes) {
+		this.zipcodes = zipcodes;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public State(Country country, List<Zip> zipcodes, String name) {
+		super();
+		this.country = country;
+		this.zipcodes = zipcodes;
+		this.name = name;
+	}
 	
 	
 }
