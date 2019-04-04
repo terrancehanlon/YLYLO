975641efa3be2ccf9803a3388cd0e62d23e67615
Terrance <terrance.hanlon@ndsu.edu>
2019-04-03 22:11:49 -0500

    add db scripts, post endpoint

--- src/main/java/com/YLYLO/controllers/AlertController.java
+++ src/main/java/com/YLYLO/controllers/AlertController.java
@@ -1,7 +1,9 @@
 package com.YLYLO.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.YLYLO.entites.User;
@@ -11,8 +13,16 @@ public class AlertController {
 
     private static final String template = "Hello ";
 
-    @RequestMapping("/alert")
-    public User alert() {
-        return new User("Volkswagen GTI", "Seattle");
+//    @RequestMapping("/alert")
+//    public User alert() {
+//        return new User("Volkswagen GTI", "Seattle");
+//    }
+//    
+    
+    @RequestMapping(value = "/alert", method = RequestMethod.GET)
+    public @ResponseBody String byParameter(@RequestParam("city") String city, @RequestParam("car") String car) {
+    	
+    	return "";
     }
+    
 }
