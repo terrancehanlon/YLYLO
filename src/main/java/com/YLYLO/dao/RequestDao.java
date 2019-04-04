48218c2bfaa28dc3ae39b892e0649ca57a24c507
Terrance <terrance.hanlon@ndsu.edu>
2019-04-03 22:02:00 -0500

    add spring dependency, add query to dao

--- src/main/java/com/YLYLO/dao/RequestDao.java
+++ src/main/java/com/YLYLO/dao/RequestDao.java
@@ -1,5 +1,19 @@
 package com.YLYLO.dao;
 
-public interface RequestDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+
+import com.YLYLO.entites.User;
+
+@Repository
+public interface RequestDao extends JpaRepository<User, Integer>{
+	
+	
+	@Query("SELECT u FROM user WHERE u.car_id = :carId and u.city_id = :cityId")
+	public ResponseEntity<User> alertUser(@Param("carId") int carId, @Param("cityId") int cityId);
+	
 
 }
